3c05edbe75acd93729d0f421d650abd6e4eacd95
zomint <zomint01@163.com>
2023-06-10 16:03:10 +0800

    update

--- index.js
+++ index.js
@@ -32,8 +32,57 @@ async function GetBranches() {
   })
 }
 
+async function createTag() {
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+  console.log(`owner: ${owner}`);
+  console.log(`repo: ${repo}`);
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+  })
+  await octokit.request(`POST /repos/${owner}/${repo}/git/tags`, {
+    owner: `${owner}`,
+    repo: `${repo}`,
+    tag: 'v0.0.1',
+    message: 'initial version',
+    object: process.env.GITHUB_TOKEN.GITHUB_SHA,
+    type: 'commit',
+    tagger: {
+      name: 'Monalisa Octocat',
+      email: 'octocat@github.com',
+      date: '2011-06-17T14:53:35-07:00'
+    },
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+}
+
+async function createRelease() {
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+  console.log(`owner: ${owner}`);
+  console.log(`repo: ${repo}`);
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+  })
+  await octokit.request(`POST /repos/${owner}/${repo}/releases`, {
+    owner: `${owner}`,
+    repo: `${repo}`,
+    tag_name: 'v1.0.0',
+    target_commitish: 'main',
+    name: 'v1.0.0',
+    body: 'Description of the release',
+    draft: false,
+    prerelease: false,
+    generate_release_notes: false,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+}
+
 try {
   GetBranches();
+  createRelease();
 } catch (error) {
   core.setFailed(error.message);
   console.log(error.message);
